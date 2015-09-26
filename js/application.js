f8ab5be8c5190faac0ca282f29964d065ffbded6
achen116 <chen.arielle@gmail.com>
2015-09-25 19:02:32 -0700

    add jquery hover for social links

--- js/application.js
+++ js/application.js
@@ -1,5 +1,16 @@
 (function() {
 	$(document).ready(function() {
-
+		socialHover();
 	})
+
+	var socialHover = function() {
+		$('.social').hover(
+		  function(){
+		    $(this).animate({fontSize: "1.5em", duration: 1000});
+		  },
+		  function(){
+		    $(this).animate({fontSize: "1em", duration: 1000});
+		  }
+		);
+	}
 })();
